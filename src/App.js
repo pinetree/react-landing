b755d318a420ef8fe349b4a4f5134e484acde709
pinetree <kirill.sosnin@gmail.com>
2018-11-09 17:32:41 +0300

    Add assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import logo from './assets/img/logo.svg'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>1</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <section className="page__wrapper rel">
+          <div className="layer-mob pc-hide" />
+          <div className="pc-hide aside-menu-mob">
+            <nav className="nav-mob">
+              <a className="nav-mob__link" href="#about">
+                About us{' '}
+              </a>
+              <a className="nav-mob__link" href="#miss">
+                Mission{' '}
+              </a>
+              <a className="nav-mob__link" href="#partner">
+                Partners{' '}
+              </a>
+              <a className="nav-mob__link" href="#faq">
+                FAQ
+              </a>
+            </nav>
+          </div>
+          <img src={logo} alt="" />
+        </section>
       </div>
     )
   }
