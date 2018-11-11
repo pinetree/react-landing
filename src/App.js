303275b2e7c55169cac43f7c10149f174f9915ca
Kirill Sosnin <kirill.sosnin@gmail.com>
2018-11-11 16:48:57 +0300

    Add nav component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import { Provider } from 'react-redux'
 import ConnectedIntlProvider from './providers/ConnectedIntlProvider'
 import { FormattedMessage } from 'react-intl'
 
-import AboutSection from './sections/About'
+import Nav from './layouts/Nav'
+import AboutSection from './layouts/sections/About'
 
 class App extends Component {
-  render() {
+  render () {
     const { store, messages } = this.props
 
     return (
@@ -16,6 +17,10 @@ class App extends Component {
         <ConnectedIntlProvider messages={messages}>
           <div className="App">
             <section className="page__wrapper rel">
+              <div className="layer-mob pc-hide" />
+              <div className="pc-hide aside-menu-mob">
+                <Nav type="nav-mob" />
+              </div>
               <AboutSection />
             </section>
 
